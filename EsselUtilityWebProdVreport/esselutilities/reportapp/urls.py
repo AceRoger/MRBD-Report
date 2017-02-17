baa7fdad1fb2c53f0dcb1b5a343801baaf5f80e3
sayursable <mayur@bynry.com>
2017-02-17 17:08:18 +0530

    added folder for html

--- EsselUtilityWebProdVreport/esselutilities/reportapp/urls.py
+++ EsselUtilityWebProdVreport/esselutilities/reportapp/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from reportapp import views
 
 urlpatterns = [
-    url(r'^records-landing-page/',views.records_landing_page,name='records-landing-page'),
+    url(r'^records-landing-page/',views.records_landing_page,name='records_landing_page'),
 
 ]
 
--- EsselUtilityWebProdVreport/esselutilities/reportapp/views.py
+++ EsselUtilityWebProdVreport/esselutilities/reportapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def records_landing_page(request):
-    return render(request, 'scheduleapp/approval.html')
+    return render(request, 'reportapp/report.html')
